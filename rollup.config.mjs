bc5e9ea3903f1ac91de359df5dc192b4af99e5c8
shuoshubao <shuoshubao@gmail.com>
2025-03-12 14:04:44 +0800

    docs: 删除 theme-chalk

---
+++ rollup.config.mjs
@@ -0,0 +1,73 @@
+/*
+ * @Author: shuoshubao
+ * @Date: 2025-03-12 11:20:35
+ * @LastEditors: shuoshubao
+ * @LastEditTime: 2025-03-12 13:36:17
+ * @Description: rollup 配置
+ */
+import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import { dirname } from 'path';
+import { defineConfig } from 'rollup';
+import babel from 'rollup-plugin-babel';
+import cleanup from 'rollup-plugin-cleanup';
+import vue from 'rollup-plugin-vue';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const plugins = [
+    vue({
+        css: false
+    }),
+    babel({
+        runtimeHelpers: true,
+        presets: ['@vue/babel-preset-jsx']
+    }),
+    alias({
+        entries: {
+            'element-ui': __dirname
+        }
+    }),
+    cleanup({
+        maxEmptyLines: 1
+    })
+];
+
+export default defineConfig([
+    {
+        input: 'src/index.js',
+        output: {
+            format: 'esm',
+            file: 'dist/index.esm.js'
+        },
+        plugins,
+        logLevel: 'silent'
+    },
+    {
+        input: 'src/index.js',
+        output: {
+            format: 'umd',
+            file: 'dist/index.umd.js',
+            name: 'ELEMENT',
+            globals: {
+                lodash: '_',
+                vue: 'Vue'
+            }
+        },
+        external: ['lodash', 'vue'],
+        plugins: [
+            ...plugins,
+            nodeResolve(),
+            commonjs(),
+            terser(),
+            replace({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            })
+        ],
+        logLevel: 'silent'
+    }
+]);
